dc0a8a997e28c5eea895eeef66b87e089d5597ff
Bror Brurberg <bror@hey.com>
2023-01-26 15:20:47 +0100

    Initial Commit

---
+++ RollIt/RollItApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollItApp.swift
+//  RollIt
+//
+//  Created by Bror2 on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RollItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
